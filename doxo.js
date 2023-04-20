9918abf73ab9c6c9c901e1d12580d628cae22f65
marcelo321 <tomibegher@gmail.com>
2023-04-20 17:56:50 -0300

    Update doxo.js

--- doxo.js
+++ doxo.js
@@ -4,9 +4,9 @@ xhr.onload = function() {
   const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g; // Regular expression to match email addresses
   const emailMatches = xhr.responseText.match(emailRegex); // Find all email addresses in the response
   if (emailMatches) {
-    console.log(`Email address found: ${emailMatches[0]}`); // Print the first email address found
+    alert(`Email address found in /settings page: ${emailMatches[0]}`); // Print the first email address found
   } else {
-    console.log('No email addresses found in the response');
+    alert('No email addresses found in the response');
   }
 };
 
